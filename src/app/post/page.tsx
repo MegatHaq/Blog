a3fe7384c0aa79e6c53dfe01dff88ea989145d04
Megat Abdul Haq <dnmegatmehat@gmail.com>
2025-01-08 22:47:40 +0800

    Added new suspense

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import { PostContainer } from "../page-components/posts-page/post-container";
 
 export default function () {
+  // add suspense for less loading time
+  const Loader = () => {
+    return <div>Loading...</div>;
+  };
   return (
     <div className="mt-[5vh] font-[family-name:var(--font-montserrat)] h-screen px-[5vw] md:w-4/5 overflow-auto homescrollbar">
       <div className="">
-        <PostContainer />
+        <Suspense fallback={<Loader />}>
+          <PostContainer />
+        </Suspense>
       </div>
     </div>
   );
